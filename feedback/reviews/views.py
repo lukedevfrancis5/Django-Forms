7ee5ee6c1fd77534d7304fa14314c98d80af1e33
Luke Francis <lukedevfrancis@gmail.com>
2023-04-24 17:28:46 -0400

    mad post request and checked if entered_name variable is empty

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponseRedirect 
 from django.shortcuts import render
 
 # views will go here 
 
 def review(request):
-    return render(request, "reviews/reviews.html")
+    if request.method == "POST":
+        entered_name = request.Post['name']
+        
+        if entered_name == "":
+            return render(request, "reviews/reviews.html", {
+                "has_error": True
+            })
+
+        print(entered_name)
+        return HttpResponseRedirect("/thank-you.html")
+     
+    return render(request, "reviews/reviews.html", {
+        "has_error": False
+    })
 
